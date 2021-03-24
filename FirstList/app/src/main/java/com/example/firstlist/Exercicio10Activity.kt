b2d7342661758b583df08166d70074b5a437bc72
carolinevianab <carolinevb123@gmail.com>
2021-03-23 21:56:06 -0300

    It was just a test

---
+++ FirstList/app/src/main/java/com/example/firstlist/Exercicio10Activity.kt
@@ -0,0 +1,11 @@
+package com.example.firstlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Exercicio10Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercicio10)
+    }
+}
